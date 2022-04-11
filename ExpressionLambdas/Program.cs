5fb249c63cda80e6e4845e5c3e80a6c27d36f439
wsr7 <wsr7@college.edu>
2022-04-11 11:32:13 +0300

    Добавьте файлы проекта.

---
+++ ExpressionLambdas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressionLambdas
+{
+    public delegate void HelloDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate hD = () => Console.WriteLine("Hello");
+
+            hD();
+
+            Console.ReadKey();
+        }
+    }
+}
